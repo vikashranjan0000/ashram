d720776ba3e9448d3148cde1dbce817d5dc4be8d
vikashranjan0000 <vikashranjan0000@gmail.com>
2020-03-20 12:55:17 +0530

    commiting Ashram changes

---
+++ contact.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <!-- Basic -->
+    <meta charset="utf-8">
+    <title>Osho</title>
+    <meta name="keywords" content="HTML5 Template" />
+    <meta name="description" content="Marvel - MultiShop Responsive HTML5 Template">
+    <meta name="author" content="pixelgeeklab.com">
+
+    <!-- Mobile Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Web Fonts  -->
+
+    <!-- Vendor CSS -->
+    <link rel="stylesheet" href="vendor/fontawesome/css/font-awesome.css">
+    <link rel="stylesheet" href="vendor/owlcarousel/owl.carousel.css" media="screen">
+    <link rel="stylesheet" href="vendor/owlcarousel/owl.theme.css" media="screen">
+    <link href="vendor/owl-carousel/owl.transitions.css" rel="stylesheet" media="screen">
+
+    <!-- Theme CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="css/theme.css">
+    <link rel="stylesheet" href="css/theme-animate.css">
+
+    <!-- Style Switcher-->
+    <link rel="stylesheet" href="style-switcher/css/style-switcher.css">
+    <link href="css/colors/default/style.css" rel="stylesheet" id="layoutstyle">
+    <link href='css/SidebarNav.min.css' media='all' rel='stylesheet' type='text/css' />
+    <script defer src="js/solid.js"></script>
+
+    <!-- Head libs -->
+    <script src="vendor/modernizr/modernizr.js"></script>
+
+    <!--[if IE]>
+      <link rel="stylesheet" href="css/ie.css">
+    <![endif]-->
+
+    <!--[if lte IE 8]>
+      <script src="vendor/respond/respond.js"></script>
+      <script src="vendor/excanvas/excanvas.js"></script>
+    <![endif]-->
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <style type="text/css">
+        body,
+        td,
+        th {
+            font-family: Dosis, sans-serif;
+            font-size: 14px;
+            color: #333;
+        }
+        
+        body {
+            background-color: #ddcdbd;
+        }
+    </style>
+</head>
+
+<body class="front">
+    <?php include_once 'public_html/includes/sideBarContent.php'; ?>
+    <div class="content container-fluid">
+
+        <div id="content">
+
+            <div class="top-contact-info1">
+                <div class="row">
+                    <div class="col-md-5"></div>
+                    <div class="col-md-7">
+                        <div class="white-from-trs wow fadeInUp">
+                            <form action="" method="get">
+
+                                <select name="" class="top-search-form">
+                                    <option>Language</option>
+                                    <option>Hindi</option>
+                                    <option>English</option>
+                                </select>
+                                <input name="" type="text" value="Keywords" onblur="if (this.value=='') this.value=this.defaultValue" onfocus="if (this.defaultValue==this.value) this.value=''" class="top-search-form1">
+                                <input name="" type="button" class="top-search-form-submit" value=".">
+                            </form>
+                            <div class="clearBoth"></div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-lg-12">
+
+                    <div class="header-bg" style="background-image:url(images/banner.jpg)">
+                        <!-- Gradient overlay -->
+                        <div class=" header-content">
+                            <div class="main-title">
+                                <!-- Main-title -->
+                                <h1 class="bold mb-30 a-f ">Contact Us</h1>
+                                <!-- breadcrumbs -->
+
+                                <!-- breadcrumbs end -->
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="inner-slide-com-cot">
+
+                    <div class="col-lg-12">
+
+                        <div style="width: 100%">
+                            <iframe width="100%" height="400" src="https://maps.google.com/maps?width=100%&amp;height=400&amp;hl=en&amp;q=1%20Grafton%20Street%2C%20Dublin%2C%20Ireland+(My%20Business%20Name)&amp;ie=UTF8&amp;t=&amp;z=14&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"> </iframe>
+                        </div>
+                        <br />
+
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="tpp-orgl21"></div>
+                <div class="inner-slide-com-cot">
+                    <div class="col-sm-6">
+                        <h2 class=" ct-fw-600">Get in Touch.</h2>
+
+                        </p>
+                        <br>
+
+                        <div class="ct-iconBox ct-iconBox--behind">
+                            <div class="ct-iconBox-icon">
+                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+
+                            </div>
+                            <div class="ct-iconBox-content">
+                                <h2 class="ct-iconBox-title  ct-fw-600">
+                              Office
+                        </h2>
+
+                                <p class="text-uppercase ct-fw-600 tt22">
+                                    Oshodhara Nanak Dham, Murthal N.H.1, 50th Milestone, GT Road, Murthal Sonepat,
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="ct-iconBox ct-iconBox--behind">
+                            <div class="ct-iconBox-icon">
+                                <i class="fa fa-phone" aria-hidden="true"></i>
+                            </div>
+                            <div class="ct-iconBox-content">
+                                <h2 class="ct-iconBox-title  ct-fw-600">
+                            Call Us
+                        </h2>
+                                <p class=" ct-fw-600 tt22">
+                                    <span class="ct-u-colorMotive">+91 </span> 0130-2483911/12
+                                </p>
+                                <p class=" ct-fw-600 tt22">
+                                    <span class="ct-u-colorMotive">+91 </span> 09671400196/3
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                        <div class="ct-iconBox ct-iconBox--behind">
+                            <div class="ct-iconBox-icon">
+                                <i class="fa fa-envelope-square" aria-hidden="true"></i>
+
+                            </div>
+                            <div class="ct-iconBox-content">
+                                <h2 class="ct-iconBox-title  ct-fw-600">
+                            Email Us
+                        </h2>
+
+                                <p class=" ct-fw-600">
+                                    <a href="#" class=" ct-u-underline ct-fw-600 tt22">info@oshodhara.org.in</a>
+                                    <br>
+                                    <br>
+
+                                </p>
+                            </div>
+                        </div>
+                        <br>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <?php include_once 'public_html/includes/footerPage.php'; ?>
+    </div>
+
+    <!-- End Search -->
+
+    <!-- Begin Login/Register -->
+
+    <!-- End Login/Register -->
+
+    <!-- Begin Quickview -->
+
+    <!-- End Quickview -->
+
+    <!-- Begin Style Switcher -->
+
+    <!-- Begin Style Switcher -->
+
+    <!-- Vendor -->
+    <script src="vendor/jquery/jquery.js"></script>
+    <script src="vendor/bootstrap/bootstrap.js"></script>
+    <script src="vendor/jquery.validation/jquery.validation.js"></script>
+    <script src="vendor/owlcarousel/owl.carousel.js"></script>
+    <script src="vendor/flexslider/jquery.flexslider-min.js"></script>
+    <script src="vendor/countdown/countdown.min.js"></script>
+    <script src="vendor/chosen/chosen.jquery.min.js"></script>
+    <script src="vendor/pricefilter/jquery.pricefilter.js"></script>
+    <script src="vendor/masonry/imagesloaded.pkgd.min.js"></script>
+    <script src="vendor/masonry/masonry.pkgd.min.js"></script>
+    <script src="vendor/uikit/uikit.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.js"></script>
+
+    <!-- Theme Base, Components and Settings -->
+    <script src="js/theme.js"></script>
+
+    <!-- Style Switcher -->
+    <script type="text/javascript" src="style-switcher/js/switcher.js"></script>
+    <script src='js/SidebarNav.min.js' type='text/javascript'></script>
+    <script>
+        $('.sidebar-menu').SidebarNav()
+    </script>
+    <script type="text/javascript">
+        < /body>
+
+        < /html>
