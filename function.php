906f536c5e0e16d7a7c47ec9e1b405d879851596
vikashranjan0000 <vikashranjan0000@gmail.com>
2020-04-18 16:46:24 +0530

    commitng changes
    ssdasd

--- function.php
+++ function.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","oshodhara");
+$con = mysqli_connect("localhost","gallerytest","CF^xv,*90,$;","oshodhara");
 	if ($con->connect_error) {
 	    die("Connection failed: " . $link->connect_error);
 	}
