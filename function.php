e7b031d366b275966cbd161c622d97080abe7307
vikashranjan0000 <vikashranjan0000@gmail.com>
2020-04-26 04:00:27 +0530

    Update function.php

--- function.php
+++ function.php
@@ -1,6 +1,5 @@
 <?php
-//$con = mysqli_connect("localhost","gallerytest","CF^xv,*90,$;","oshodhara");
-$con = mysqli_connect("localhost","root","","oshodhara");
+$con = mysqli_connect("localhost","gallerytest","CF^xv,*90,$;","oshodhara");
 	if ($con->connect_error) {
 	    die("Connection failed: " . $link->connect_error);
 	}
